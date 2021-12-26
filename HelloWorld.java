64d4b79af5ad45f52c8e529c7e645557dbd09794
yanghaitao <yanghaitao1995@aliyun.com>
2021-12-26 23:14:48 +0800

    The root commit for HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * Description:
+ *             This is a test Java code for git and github
+*/
+
+// The java main method
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("hello world v-0.1 based on Java");
+	}
+}
+
