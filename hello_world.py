a2a687bd29ff7844bd91b4c14d8f7df859e40e3b
yanghaitao <yanghaitao1995@aliyun.com>
2021-12-26 23:13:46 +0800

    Adding the language description and modify explanation

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,8 @@
 # -*- coding=utf-8 -*-
 # hello_world.py
 
-# This is a test code for git and github
-print("hello world v-0.1")
+# This is a test Python code for git and github
+print("hello world v-0.1 based on Python")
 print("+" * 30)
-print("hello world v-0.2")
+print("hello world v-0.2 based on Python")
 
