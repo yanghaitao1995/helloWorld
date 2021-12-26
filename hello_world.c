4564dd9569ec475a57d4dfd1eadb427cccda8373
yanghaitao <yanghaitao1995@aliyun.com>
2021-12-26 23:15:20 +0800

    The root commit for hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,20 @@
+/*
+ * Description:
+ *             This is a test C code for git and github
+*/
+
+// preprocessing command
+// To avoid c4996 warning
+// #define _CRT_SECURE_NO_WARNINGS
+// #pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+
+// The C main function
+int main()
+{
+	printf("hello world v-0.1 based on C\n");
+	system("pause");
+	return 0;
+}
+
